a7ba5a3ca2508a814557fb9f42d31d32c183e89f
junpatch <junya.ishimoto@gmail.com>
2025-03-07 15:19:22 +0900

    debug: ルート調査

--- app/routes/main_routes.py
+++ app/routes/main_routes.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Blueprint, render_template
 
 bp = Blueprint("main", __name__)
@@ -6,3 +8,8 @@ bp = Blueprint("main", __name__)
 @bp.route("/")
 def index():
     return render_template("index.html")
+
+@bp.route("/test")
+def test():
+    os.getcwd()
+    return os.getcwd()
